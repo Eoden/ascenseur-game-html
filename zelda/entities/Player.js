b884846336e9ad84137f167a925d337d6602eb6d
Eoden <95504@supinfo.com>
2026-02-18 17:27:59 +0700

    fix: restore Player export and single sprite image property

--- zelda/entities/Player.js
+++ zelda/entities/Player.js
@@ -1,18 +1,12 @@
-export default class Player {
+export class Player {
   constructor(x, y) {
     this.x = x;
     this.y = y;
     this.speed = 3;
     this.size = 32;
 
-    this.image1 = new Image();
-    this.image1.src = "assets/sprites/hero_walk_right_sheet.png";
-
-    this.image2 = new Image();
-    this.image2.src = "./assets/sprites/hero_walk_right_sheet.png";
-
-    this.image3 = new Image();
-    this.image3.src = "zelda/assets/sprites/hero_walk_right_sheet.png";
+    this.image = new Image();
+    this.image.src = "assets/sprites/hero_walk_right_sheet.png";
   }
 
   update(input) {
@@ -22,27 +16,7 @@ export default class Player {
     if (input.down) this.y += this.speed;
   }
 
-  render(ctx) {
-    let drawn = false;
-
-    if (this.image1.complete && this.image1.naturalWidth > 0) {
-      ctx.drawImage(this.image1, this.x, this.y, this.size, this.size);
-      drawn = true;
-    }
-
-    if (this.image2.complete && this.image2.naturalWidth > 0) {
-      ctx.drawImage(this.image2, this.x + 40, this.y, this.size, this.size);
-      drawn = true;
-    }
-
-    if (this.image3.complete && this.image3.naturalWidth > 0) {
-      ctx.drawImage(this.image3, this.x + 80, this.y, this.size, this.size);
-      drawn = true;
-    }
-
-    if (!drawn) {
-      ctx.fillStyle = "red";
-      ctx.fillRect(this.x, this.y, this.size, this.size);
-    }
+  attack() {
+    // placeholder
   }
 }
